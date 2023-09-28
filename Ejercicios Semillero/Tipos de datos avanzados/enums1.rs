12b0f5b1efee323b9d6f934481ad067933e58530
Samuel Herrera Marín <129117837+LairezMk@users.noreply.github.com>
2023-09-28 16:42:46 -0500

    Add files via upload

---
+++ Ejercicios Semillero/Tipos de datos avanzados/enums1.rs
@@ -0,0 +1,24 @@
+// I AM NOT DONE
+
+#[derive(Debug)]
+enum Message {
+    // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
+}
+
+#[cfg(test)]
+mod tests {
+ use super::*;   
+    #[test]
+    fn call_enum() {
+        println!("{:?}", Message::Quit);
+        println!("{:?}", Message::Echo);
+        println!("{:?}", Message::Move);
+        println!("{:?}", Message::ChangeColor);    
+    }
+
+}
+
